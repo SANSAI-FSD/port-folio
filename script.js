ead865e4f038c431a2b31be3768d5f6c0a304762
SANSAI-FSD <SANSAISANSAI2003@GMAIL.COM>
2025-06-19 13:02:11 +0530

    images

--- script.js
+++ script.js
@@ -33,68 +33,68 @@
 
 
 
-// /*=============== SHOW SCROLL UP ===============*/ 
-// const scrollUp = () =>{
-// 	const scrollUp = document.getElementById('scroll-up')
-//     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-// 	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-// 						: scrollUp.classList.remove('show-scroll')
-// }
-// window.addEventListener('scroll', scrollUp)
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
-// /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-// const sections = document.querySelectorAll('section[id]')
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
     
-// const scrollActive = () =>{
-//   	const scrollDown = window.scrollY
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
-// 	sections.forEach(current =>{
-// 		const sectionHeight = current.offsetHeight,
-// 			  sectionTop = current.offsetTop - 58,
-// 			  sectionId = current.getAttribute('id'),
-// 			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-// 		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-// 			sectionsClass.classList.add('active-link')
-// 		}else{
-// 			sectionsClass.classList.remove('active-link')
-// 		}                                                    
-// 	})
-// }
-// window.addEventListener('scroll', scrollActive)
-
-
-
-// // Activate / deactivate the theme manually with the button
-// themeButton.addEventListener('click', () => {
-//     // Add or remove the dark / icon theme
-//     document.body.classList.toggle(darkTheme)
-//     themeButton.classList.toggle(iconTheme)
-//     // We save the theme and the current icon that the user chose
-//     localStorage.setItem('selected-theme', getCurrentTheme())
-//     localStorage.setItem('selected-icon', getCurrentIcon())
-// })
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 
 
-// // jssssss
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     const projectCards = document.querySelectorAll(".projects__card");
 
-//     const observer = new IntersectionObserver((entries, observer) => {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//                 entry.target.classList.add("fade-in");
-//                 observer.unobserve(entry.target);
-//             }
-//         });
-//     }, { threshold: 0.2 });
 
-//     projectCards.forEach(card => {
-//         observer.observe(card);
-//     });
-// });
+// jssssss
+
+document.addEventListener("DOMContentLoaded", function () {
+    const projectCards = document.querySelectorAll(".projects__card");
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("fade-in");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.2 });
+
+    projectCards.forEach(card => {
+        observer.observe(card);
+    });
+});
 
 
 
